04719e8fbb30ea619cc7eb01ba826fe8f75b3e6a
scott harvey <donzail@hotmail.com>
2020-11-04 20:55:54 -0500

    Work In Progress - Timeline
    Working on mouseovers and show / hide. Highly unstyled at this point
    
    Unfortunately I didn't finish everything.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,7 +1,14 @@
 function hideTimelineDetail(){
-    document.getElementsByClassName("timeline-week").display = "none";
+    // this wasn't working - went with awful hardcode
+    //document.getElementsByClassName("timeline-week").style['display'] =  "none";
+
+    ['timeline-week0', 'timeline-week1', 'timeline-week2', 'timeline-week3', 'timeline-week4', 'timeline-week5', 'timeline-week6', 'timeline-week7', 'timeline-week8', 'timeline-week9', 'timeline-week10', 'timeline-week11'].forEach(
+        (item, index) => {
+            document.getElementById(item).style.display =  "none";
+        }
+    )
 }
 
-/* function showTimelineDetail(id){
-    document.getElementById(id).display = "block";
-} */
+function showTimelineDetail(id){
+    document.getElementById(id).style.display =  "block";
+}
